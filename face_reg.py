876cf67937609b5d6798942914b18d41f038acf7
duyvu1109 <duyvu1109@gmail.com>
2021-10-02 11:11:01 +0700

    storeUserImage

--- face_reg.py
+++ face_reg.py
@@ -1,29 +1,30 @@
 # import opencv
 import cv2
-import sqlite3
+import os
 
-# def insertUsers2DB():
-#     pass
-
-# Using library facedetect written by opencv
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# Open webcam
-video = cv2.VideoCapture(0)
-# Main
-while True:
-    # Read data from webcam
-    check, data = video.read()
-    # Detect data
-    faces = face_cascade.detectMultiScale(data)
-    for x,y,w,h in faces:
-        # Draw rect around faces
-        frame = cv2.rectangle(data, (x,y), (x+w,y+h), (0,255,0), 3)
-    # Show the console
-    cv2.imshow('Face Detect', data)
-    key = cv2.waitKey(1)
-    # Press 'q' to exit
-    if key == ord('q'):
-        break
-# Release memory
-video.release()
+def storeUserImage(name):
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    video = cv2.VideoCapture(0)
+    count = 0
+    os.makedirs('user_capture/' + str(name))
+    while True:
+        check, data = video.read()
+        faces = face_cascade.detectMultiScale(data)
+        for x,y,w,h in faces:
+            
+            cv2.rectangle(data, (x,y), (x+w,y+h), (0,255,0), 3)
+            cv2.imwrite('user_capture/' + str(name) + '/' + '.' + str(count) + '.jpg', data)
+            count += 1
+        
+        cv2.waitKey(3)
+        cv2.imshow('Face Detect', data)
+        key = cv2.waitKey(1)
+        # Press 'q' to exit
+        if key == ord('q'):
+            break
+        elif count == 10:
+            break
+    # Release memory
+    video.release()
 cv2.destroyAllWindows()
+
