86f528571d46fecbc9cff3c304ba5e560a88cd27
duyvu1109 <duyvu1109@gmail.com>
2021-10-02 23:10:11 +0700

    run to query from DB

--- mongodb.py
+++ mongodb.py
@@ -2,7 +2,7 @@
 THIS MODULE IS USED TO CONNECT TO DB via MongoDB
 '''
 from pymongo import MongoClient
-
+import os
 '''
 Using Mongodb to store Data:
 My DB named "DoorLock" has two Collections:
@@ -18,3 +18,23 @@ db = cluster["DoorLock"]
 users = db["User"]
 # open History collection
 history = db ["History"]
+
+def queryFromDB():
+    if not os.path.exists('users.json') and not os.path.exists('history.json'):
+        open('users.json', 'a').close()
+        open('history.json', 'a').close()
+    from bson.json_util import dumps
+    UsersData = users.find()
+    HistoryData = history.find()
+    listUser = list(UsersData)
+    listHistory = list(HistoryData)
+    # Converting to the JSON
+    json_data = dumps(listUser, indent = 2) 
+    with open('users.json', 'w') as file:
+        file.write(json_data)
+    file.close()
+    json_data = dumps(listHistory, indent = 2) 
+    with open('history.json', 'w') as file:
+        file.write(json_data)
+    file.close()
+queryFromDB()
