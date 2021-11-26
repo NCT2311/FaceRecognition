688f0de0a51657f9bff1edc9a586b2e13ffb4e6d
NCT <thanh.nguyen231101@hcmut.edu.vn>
2021-11-27 02:01:01 +0700

    Done Merge

---
+++ src_2/mail_sending.py
@@ -0,0 +1,27 @@
+"""
+    This module used to send email automatic via email-bot chat named "doorlock.bot",
+    
+    Email: doorlock.bot@gmail.com
+    Password: datkll211
+"""
+import smtplib, ssl
+
+
+def sendMail(link):
+    port = 465
+
+    sender = "doorlock.bot@gmail.com"
+    password = "datkll211"
+
+    recieve = "nguyencongthanh23111@gmail.com"
+
+    message = "abc"
+
+    context = ssl.create_default_context()
+
+    print("Starting to send")
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender, password)
+        server.sendmail(sender, recieve, message)
+
+    print("Sent email!")
---
+++ src_2/mongodb.py
@@ -0,0 +1,71 @@
+'''
+THIS MODULE IS USED TO CONNECT TO DB via MongoDB
+'''
+from pymongo import MongoClient
+import os
+from time import sleep
+'''
+Using Mongodb to store Data:
+My DB named "DoorLock" has two Collections:
+    + Users: store userInfo(_id, name, priority).
+    + History: store userEvent(add new user, remove user).
+CONNECTING STRING FORM: "mongodb+srv://<USER>:<PASSWORD>@<CLUSTER>/<user>?ssl=true&ssl_cert_reqs=CERT_NONE"
+'''
+
+# connect to DB
+cluster = MongoClient("mongodb+srv://hda1010:duyanh123@cluster0.ukowb.mongodb.net/facerecognition?retryWrites=true&w=majority")
+# open DoorLock
+db = cluster["facerecognition"]
+# open User collection
+persons = db["persons"]
+# open History collection
+turns = db["turns"]
+
+#
+
+def queryFromDB():
+    if not os.path.exists('users.json') and not os.path.exists('history.json'):
+        open('persons.json', 'a').close()
+        open('turns.json', 'a').close()
+    from bson.json_util import dumps
+    UsersData = persons.find()
+    HistoryData = turns.find()
+    listUser = list(UsersData)
+    listHistory = list(HistoryData)
+    # Converting to the JSON
+    json_data = dumps(listUser, indent = 2) 
+    with open('persons.json', 'w') as file:
+        file.write(json_data)
+    file.close()
+    json_data = dumps(listHistory, indent = 2) 
+    with open('turns.json', 'w') as file:
+        file.write(json_data)
+    file.close()
+
+# queryFromDB()
+
+# get response from DB
+def receiveResponse():
+    response = False
+    for timeRemain in range(60):
+        '''Query from DB to get response'''
+        response = turns.find_one({}, {id: 0}).response
+        if (response):
+            return '''Door open'''
+        sleep(5)
+    return '''Door still lock'''
+
+# get Person ID
+def searchByName(name):
+    for entry in persons.find():
+        if entry.name == name:
+            return entry.get('_id')
+    return '''Can't find'''
+
+# open Flag collection
+flag = db["flags"]
+def updateFlag():
+    f = flag.find_one()
+    newFlag = {"$set":{"Flagcheck": False}}
+    flag.update_one(f, newFlag)
+    pass
