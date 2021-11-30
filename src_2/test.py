62b24a66fec0dc3e7dbb71198ccf955cb0c690fd
duyvu1109 <duyvu1109@gmail.com>
2021-11-30 18:42:17 +0700

    .

--- src_2/test.py
+++ src_2/test.py
@@ -1,5 +1,57 @@
 import cv2
+import os
+import numpy as np
+from PIL import Image
 import pickle
+from time import sleep
+
+def training():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    image_dir = os.path.join(BASE_DIR, "data")
+
+    face_cascade = cv2.CascadeClassifier("./cascades/data/haarcascade_frontalface_alt2.xml")
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+    current_id = 0
+    label_ids = {}
+    y_labels = []
+    x_train = []
+
+    for root, dirs, files in os.walk(image_dir):
+        for file in files:
+            if file.endswith("png") or file.endswith("jpg"):
+                path = os.path.join(root, file)
+                label = (os.path.basename(root).replace(" ", "-").lower())  # os.path.dirname(path) == root
+                if not label in label_ids:
+                    label_ids[label] = current_id
+                    current_id += 1
+                id_ = label_ids[label]
+
+                pil_image = Image.open(path).convert("L")  # grayscale
+                size = (550, 550)
+                final_image = pil_image.resize(size, Image.ANTIALIAS)
+                image_array = np.array(final_image, "uint8")
+                # print(image_array)
+                faces = face_cascade.detectMultiScale(
+                    image_array, scaleFactor=1.5, minNeighbors=5
+                )
+                for (x, y, w, h) in faces:
+                    roi = image_array[y : y + h, x : x + w]
+                    x_train.append(roi)
+                    y_labels.append(id_)
+
+    with open("labels.pickle", "wb") as f:
+        pickle.dump(label_ids, f)
+
+    recognizer.train(x_train, np.array(y_labels))
+    recognizer.save("trainner.yml")
+
+    print('Traning done.')
+
+# training()
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
 
 face_cascade = cv2.CascadeClassifier("./cascades/data/haarcascade_frontalface_alt2.xml")
 
@@ -23,13 +75,19 @@ flag = 0
 person_name1, person_name2 = "", ""
 make_720p()
 
-n = 5
-
+n = 50
 run = True
 
+# 'src_2\data\congthanh\0.jpg'
+# img = cv2.imread("src_2\data\congthanh\0.jpg")
+# cv2.imshow("img", img)
+# sleep(5)
+
 def person1(name):
-    for index in range(n):
-        img = cv2.imread(".\\images\\{}\\{}.jpg".format(name, index), cv2.IMREAD_COLOR)
+    Path = ".\\data\\{}".format(name)
+    for filename in os.listdir(Path):
+        print(".\\data\\{}\\{}".format(name, filename))
+        img = cv2.imread(".\data\{}\{}".format(name, filename), cv2.IMREAD_COLOR)
         frame = img
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # Cascade face_cascade
@@ -41,8 +99,8 @@ def person1(name):
             if conf >= 45:
                 temp_id = id_
                 person_name1 = labels[id_]
-                if person_name1 != '':
-                    return person_name1
+                # if person_name1 != '':
+                #     return person_name1
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 name = labels[id_]
                 color = (255, 255, 255)
@@ -53,7 +111,11 @@ def person1(name):
             end_cord_x = x + w  # Width of Rectangle
             end_cord_y = y + h  # Height of Recctangle
             cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+        cv2.imshow("frame", frame)
+        sleep(1)
     return 'cannot found'
+
+# person1('congthanh')
 
 def person2(name):
     for index in range(n):
@@ -69,8 +131,8 @@ def person2(name):
             if conf >= 45:
                 temp_id = id_
                 person_name2 = labels[id_]
-                if person_name2 != '':
-                    return person_name2
+                # if person_name2 != '':
+                #     return person_name2
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 name = labels[id_]
                 color = (255, 255, 255)
@@ -81,12 +143,27 @@ def person2(name):
             end_cord_x = x + w  # Width of Rectangle
             end_cord_y = y + h  # Height of Recctangle
             cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+        cv2.imshow("img", img)
     return 'cannot found'
+person2('congthanh')
 
-print("person1: {0}\nperson2: {1}\nResult: {2}".format(
-    person1('61a4ededdd2f672c6d602ef6'), 
-    person2('congthanh'),
-    person1('61a4ededdd2f672c6d602ef6') != person2('congthanh')))
+def iterativeDataSet(name):
+    '''compare name with another'''
+    # path = ".\\data\\{}".format(name)
+    folderPath = ".\\data"
+    for folderName in os.listdir(folderPath):
+        childPath = ".\\data\\{}".format(folderName)
+        for filename in os.listdir(childPath):
+            person1(str(filename))
+        # print(os.path.join(directory, filename))
+    # img = cv2.imread(".\\images\\{}\\*.jpg".format(name, index), cv2.IMREAD_COLOR)
+
+# iterativeDataSet('congthanh')
+
+# print("person1: {0}\nperson2: {1}\nResult: {2}".format(
+#     person1('61a4ededdd2f672c6d602ef6'), 
+#     person2('congthanh'),
+#     person1('61a4ededdd2f672c6d602ef6') != person2('congthanh')))
 
 cap.release()
 cv2.destroyAllWindows()
