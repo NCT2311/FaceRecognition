865a1edd050558ad1777972916c72153aead4bad
NCT <thanh.nguyen231101@hcmut.edu.vn>
2021-12-03 17:20:13 +0700

    Done create custom face

--- src_2/faces-create.py
+++ src_2/faces-create.py
@@ -5,6 +5,7 @@ import cv2
 import os
 import pymongo
 from modules import persons, Control
+import time
 
 
 def newPerson():
@@ -14,7 +15,7 @@ def newPerson():
     id = persons.find().sort("_id", pymongo.DESCENDING).limit(1)[0]["_id"]
     # creater new directory in user_capture
     os.makedirs("./images/" + str(id))
-    return "Add {} to dataset".format(Fname + Lname), str(id)
+    return "Adding {} to dataset".format(Fname + Lname), str(id)
 
 
 def storeUserImage():
@@ -26,19 +27,27 @@ def storeUserImage():
     )
     video = cv2.VideoCapture(0)
     count = 0
+    count_shot = 15
     while True:
         check, data = video.read()
         faces = face_cascade.detectMultiScale(data, scaleFactor=1.5, minNeighbors=5)
         for x, y, w, h in faces:
-            # cv2.rectangle(data, (x, y), (x + w, y + h), (0, 255, 0), 3)
-            cv2.imwrite("./images/" + newDataSet + "/" + str(count) + ".jpg", data)
-            count += 1
+            cv2.rectangle(data, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            if count_shot == 0:
+                cv2.imwrite("./images/" + newDataSet + "/" + str(count) + ".jpg", data)
+                count += 1
+                count_shot = 15
+            count_shot = count_shot - 1
         cv2.waitKey(3)
         cv2.imshow("Face Detect", data)
         key = cv2.waitKey(1)
 
+        # if key == ord("k"):
+        #     cv2.imwrite("./images/" + newDataSet + "/" + str(count) + ".jpg", data)
+        #     count += 1
+
         # Press 'q' to exit
-        if key == ord("q") or count == 65:
+        if key == ord("q") or count == 20:
             break
     print("added!")
     # Release memory
